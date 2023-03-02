f0bcfb1d1eaa52d7532163503049e0f9831c4d52
Antelo97 <oscaran_97@hotmail.com>
2023-03-02 13:54:49 +0100

    a

---
+++ PantryManager/PantryManager/src/main/java/com/example/pantrymanager/PantryManager/PantryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pantrymanager.PantryManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantryManagerApplication.class, args);
+	}
+
+}
